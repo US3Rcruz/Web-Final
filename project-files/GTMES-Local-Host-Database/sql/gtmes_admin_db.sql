e316c40abe5d56f7ab7e62191a5a023d58929381
US3Rcruz <116894008+US3Rcruz@users.noreply.github.com>
2025-02-06 20:46:10 +0800

    Update gtmes_admin_db.sql

--- project-files/GTMES-Local-Host-Database/sql/gtmes_admin_db.sql
+++ project-files/GTMES-Local-Host-Database/sql/gtmes_admin_db.sql
@@ -38,8 +38,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee');
+;
 
 --
 -- Indexes for dumped tables
